f84fd3fff6b02087bb42cde1cc86fed5254c82f8
Arthur Gautier <aga@zenexity.com>
2013-03-18 09:39:30 +0100

    Adds publishing

--- debian/.gbc/publish.sh
+++ debian/.gbc/publish.sh
@@ -1,7 +1,25 @@
 #!/bin/sh
 
-echo "DIST: $1"
+here=$(cd `dirname $0`; pwd)
 
+case $1 in
+  squeeze)
+    repo=gbc-stable
+    ;;
+  squeeze-testing)
+    repo=gbc-testing
+    ;;
+  squeeze-unstable)
+    repo=gbc-unstable
+    ;;
+  *)
+    echo "$1 not handled"
+    exit 1
+esac
+
+dist=$1
 shift
 
 debsign -k707EFD41 $*
+
+dput --config $here/dput.cf $dist $*
